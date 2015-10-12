57050a8faf2694200f98cc9ae035dda747061a2d
Max Lang <max@cockroachlabs.com>
2015-10-12 18:10:05 -0400

    number viz

--- ui/ts/components/visualizations/number.ts
+++ ui/ts/components/visualizations/number.ts
@@ -1,8 +1,33 @@
-/// <reference path="../external/mithril/mithril.d.ts" />
-/// <reference path="../../typings/d3/d3.d.ts" />
+/// <reference path="../../external/mithril/mithril.d.ts" />
+/// <reference path="../../../typings/d3/d3.d.ts" />
 
 // Author: Max Lang (max@cockroachlabs.com)
 
 module Visualizations {
   "use strict";
+
+  import MithrilVirtualElement = _mithril.MithrilVirtualElement;
+
+  let formatFn: (n: number) => string;
+
+  export module NumberVisualization {
+    export function controller(): any {
+      return {};
+    }
+
+    export function view(ctrl: any, info: VisualizationInfo): MithrilVirtualElement {
+
+      formatFn = info.visualizationArguments.formatFn || d3.format(info.visualizationArguments.format || ".3s");
+
+      let data: VisualizationData = _.isArray(info.data) ? info.data[0] : info.data;
+
+      return m(
+        ".visualization",
+        {style: "width:" + info.width.toString() + "px; height:" + info.height.toString() + "px; " +
+          "line-height:" + info.height.toString() + "px; " },
+        [
+          m(".number", {style: "zoom:" + (info.visualizationArguments.zoom || "100%") + ";"}, formatFn(data.value))
+        ]);
+    }
+  }
 }
--- ui/ts/components/visualizations/pie.ts
+++ ui/ts/components/visualizations/pie.ts
@@ -1,8 +1,13 @@
-/// <reference path="../external/mithril/mithril.d.ts" />
-/// <reference path="../../typings/d3/d3.d.ts" />
+/// <reference path="../../external/mithril/mithril.d.ts" />
+/// <reference path="../../../typings/d3/d3.d.ts" />
 
 // Author: Max Lang (max@cockroachlabs.com)
 
 module Visualizations {
   "use strict";
+
+  export module PieVisualization {
+
+  }
+
 }
